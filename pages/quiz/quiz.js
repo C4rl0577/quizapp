782aced077a94194965fc7f27d4d644c0be27753
Carlos Eduardo Morais Costa Ciência da computação <C4rlcm.234@gmail.com>
2024-05-27 19:47:29 +0000

    Adicionada a função de validar a resposta do usuário.

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -7,6 +7,9 @@ const assunto = localStorage.getItem("assunto")
 let quiz = {}
 let pontos = 0
 let pergunta = 1
+let resposta = ""
+let idInputResposta = ""
+let respostaCorretaid = ""
 
 botaoTema.addEventListener("click", () => {
     trocarTema(body, botaoTema)
@@ -105,8 +108,22 @@ function alterarSinais(texto) {
     return texto.replace(/</g, "&lt;").replace(/>/g, "&gt;")
 }
 
-function guardarResposta() {
-    console.log("gardar resposta")
+function guardarResposta(evento) {
+    resposta = evento.target.value
+    idInputResposta = evento.target.id
+
+    const botaoEnviar = document.querySelector(".alternativas button")
+    botaoEnviar.addEventListener("click", validarResposta)
+}
+
+function validarResposta() {
+    if (resposta === quiz.questions[pergunta-1].answer) {
+        document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "correta")
+        pontos = pontos + 1
+    } else {
+        document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "errada")
+        document.querySelector(`label[for='${respostaCorretaid}']`).setAttribute("id", "correta")
+    }
 }
 
 async function iniciar() {
@@ -117,6 +134,10 @@ async function iniciar() {
     const inputsResposta = document.querySelectorAll(".alternativas input")
     inputsResposta.forEach(input => {
         input.addEventListener("click", guardarResposta)
+
+        if (input.value === quiz.questions[pergunta-1].answer) {
+            respostaCorretaid = input.id
+        }
     })
 }
 
